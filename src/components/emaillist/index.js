7acedf6ee0100b36267ce855bc74eb6102f5c439
Shaikh Muzaker <shaikhmuzaker@gmail.com>
2020-05-13 12:06:18 +0530

    refactor from class component to functional components with react hooks

--- src/components/emaillist/index.js
+++ src/components/emaillist/index.js
@@ -1,32 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // import axios from "axios";
 import "../../styles/emaillist.scss";
 import EmailView from "../emailview";
 import Header from "../header";
 
-class EmailList extends React.Component {
-  state = {
-    emailList: [],
-    stateId: false,
-  };
-  componentDidMount() {
-    this.loadList();
-  }
+const EmailList = () => {
+  const [emailList, emailValue] = useState([]);
+  const [stateId, stateValue] = useState(false);
 
-  loadList = () => {
+  useEffect(() => {
+    loadList();
+  }, []);
+
+  const loadList = () => {
     fetch("https://flipkart-email-mock.now.sh/")
       .then((res) => {
         return res.json();
       })
       .then((data) => {
         console.log(data);
-        this.setState({
-          emailList: data.list,
-        });
+        emailValue(data.list);
       });
   };
 
-  listId = (id) => {
+  const listId = (id) => {
     console.log("inside id", id);
     fetch(`https://flipkart-email-mock.now.sh/?id=${id}`)
       .then((res) => {
@@ -34,60 +31,140 @@ class EmailList extends React.Component {
       })
       .then((data) => {
         console.log("details", data);
-        this.setState({
-          stateId: data,
-        });
+        stateValue(data);
       });
   };
 
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <div className={"main-list"}>
-          <div className={this.state.stateId ? "card-div" : "new-list"}>
-            {this.state.emailList.map((listData) => {
-              return (
-                <div
-                  onClick={() => this.listId(listData.id)}
-                  className="list-card"
-                  key={listData.id}
-                >
-                  <div className="avatar">
-                    <span>{listData.from.email[0]}</span>
-                  </div>
-                  <div className="inner-list">
-                    <h3>
-                      From:{" "}
-                      <b>
-                        {listData.from.name} {"<"}
-                        {listData.from.email}
-                        {">"}
-                      </b>
-                    </h3>
-                    <h3>
-                      Subject: <b>{listData.subject}</b>
-                    </h3>
-                    <p>{listData.short_description}</p>
-                    <p>
-                      {Intl.DateTimeFormat("en-US", {
-                        day: "2-digit",
-                        month: "2-digit",
-                        year: "2-digit",
-                      }).format(listData.date)}
-                    </p>
-                  </div>
-                  {/* <EmailView id={this.state.id} /> */}
+  return (
+    <React.Fragment>
+      <Header />
+      <div className={"main-list"}>
+        <div className={stateId ? "card-div" : "new-list"}>
+          {emailList.map((listData) => {
+            return (
+              <div
+                onClick={() => listId(listData.id)}
+                className="list-card"
+                key={listData.id}
+              >
+                <div className="avatar">
+                  <span>{listData.from.email[0]}</span>
                 </div>
-              );
-            })}
-          </div>
-          {this.state.stateId ? <EmailView id={this.state.stateId} /> : null}
-          <div></div>
+                <div className="inner-list">
+                  <h3>
+                    From:{" "}
+                    <b>
+                      {listData.from.name} {"<"}
+                      {listData.from.email}
+                      {">"}
+                    </b>
+                  </h3>
+                  <h3>
+                    Subject: <b>{listData.subject}</b>
+                  </h3>
+                  <p>{listData.short_description}</p>
+                  <p>
+                    {Intl.DateTimeFormat("en-US", {
+                      day: "2-digit",
+                      month: "2-digit",
+                      year: "2-digit",
+                    }).format(listData.date)}
+                  </p>
+                </div>
+                {/* <EmailView id={id} /> */}
+              </div>
+            );
+          })}
         </div>
-      </React.Fragment>
-    );
-  }
-}
+        {stateId ? <EmailView id={stateId} /> : null}
+      </div>
+    </React.Fragment>
+  );
+};
+
+// class EmailList extends React.Component {
+//   state = {
+//     emailList: [],
+//     stateId: false,
+//   };
+//   componentDidMount() {
+//     this.loadList();
+//   }
+
+//   loadList = () => {
+//     fetch("https://flipkart-email-mock.now.sh/")
+//       .then((res) => {
+//         return res.json();
+//       })
+//       .then((data) => {
+//         console.log(data);
+//         this.setState({
+//           emailList: data.list,
+//         });
+//       });
+//   };
+
+//   listId = (id) => {
+//     console.log("inside id", id);
+//     fetch(`https://flipkart-email-mock.now.sh/?id=${id}`)
+//       .then((res) => {
+//         return res.json();
+//       })
+//       .then((data) => {
+//         console.log("details", data);
+//         this.setState({
+//           stateId: data,
+//         });
+//       });
+//   };
+
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <Header />
+//         <div className={"main-list"}>
+//           <div className={this.state.stateId ? "card-div" : "new-list"}>
+//             {this.state.emailList.map((listData) => {
+//               return (
+//                 <div
+//                   onClick={() => this.listId(listData.id)}
+//                   className="list-card"
+//                   key={listData.id}
+//                 >
+//                   <div className="avatar">
+//                     <span>{listData.from.email[0]}</span>
+//                   </div>
+//                   <div className="inner-list">
+//                     <h3>
+//                       From:{" "}
+//                       <b>
+//                         {listData.from.name} {"<"}
+//                         {listData.from.email}
+//                         {">"}
+//                       </b>
+//                     </h3>
+//                     <h3>
+//                       Subject: <b>{listData.subject}</b>
+//                     </h3>
+//                     <p>{listData.short_description}</p>
+//                     <p>
+//                       {Intl.DateTimeFormat("en-US", {
+//                         day: "2-digit",
+//                         month: "2-digit",
+//                         year: "2-digit",
+//                       }).format(listData.date)}
+//                     </p>
+//                   </div>
+//                   {/* <EmailView id={this.state.id} /> */}
+//                 </div>
+//               );
+//             })}
+//           </div>
+//           {this.state.stateId ? <EmailView id={this.state.stateId} /> : null}
+//         </div>
+//       </React.Fragment>
+//     );
+//   }
+// }
 
 export default EmailList;
--- src/components/emailview/index.js
+++ src/components/emailview/index.js
@@ -1,24 +1,22 @@
 import React from "react";
 import "../../styles/emaillist.scss";
 
-class EmailView extends React.Component {
-  render() {
-    console.log(this.props.id);
-    return (
-      <div className="email-view">
-        {this.props.id ? (
-          <div>
-            <div className="favorite">
-              <button>Mark as Favorite</button>
-            </div>
-            <p>{this.props.id.body}</p>
+const EmailView = ({ id }) => {
+  console.log(id);
+  return (
+    <div className="email-view">
+      {id ? (
+        <div>
+          <div className="favorite">
+            <button>Mark as Favorite</button>
           </div>
-        ) : (
-          <div>Loading post...</div>
-        )}
-      </div>
-    );
-  }
-}
+          <p>{id.body}</p>
+        </div>
+      ) : (
+        <div>Loading post...</div>
+      )}
+    </div>
+  );
+};
 
 export default EmailView;
